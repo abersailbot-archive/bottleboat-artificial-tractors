60162e1be43b29a261731f5244971a59d94f39e1
Taylor Brown <tab23@cs-root.dcs.aber.ac.uk>
2016-10-26 16:01:59 +0100

    Add basic comments about control

---
+++ code/code.ino
@@ -0,0 +1,18 @@
+
+void setup() {
+
+Serial.begin(9600); 
+
+//Calibrate compass for North. 
+//Input from compass
+//If compass is between 1 and 1800 degrees, rotate propeller direction 
+
+}
+
+
+void loop() {
+
+
+  
+}
+
