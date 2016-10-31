24f2b7eff6699cd9aeaa11e77886538cbe614e1b
Taylor Brown <tab23@cs-root.dcs.aber.ac.uk>
2016-10-31 15:27:20 +0000

    Add comments about compass

--- code/code.ino
+++ code/code.ino
@@ -4,8 +4,27 @@ void setup() {
 Serial.begin(9600); 
 
 //Calibrate compass for North. 
+
 //Input from compass
-//If compass is between 1 and 1800 degrees, rotate propeller direction 
+
+//Read input from compass on the arduino
+    /* pseudocode 
+ PID:
+ Error = Setpoint - Actual
+ Integral = Integral + (Error*dt)
+ Derivative = (Error - Previous_error)/dt
+ Drive = (Error*kP) + (Integral*kI) + (Derivative*kD)
+ Previous_error = Error
+ wait(dt)
+GOTO PID 
+     */
+    
+//If compass is between 1 and 1800 degrees, rotate propeller direction
+
+//If at 0 degrees, no change in propeller direction
+
+
+
 
 }
 
